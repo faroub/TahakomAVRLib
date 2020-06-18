5d3680da56f760e0b8554028b57b10c8619b63fa
Farid Oubbati <f.oubbati@yahoo.f>
2020-06-18 16:35:33 +0200

    add src/ folder

---
+++ src/src_mcu/apps_mcu/blink_led_mcu/main.cpp
@@ -0,0 +1,21 @@
+/**
+ * @brief Simple application to blink a Led
+ * @author Farid Oubbati (https://github.com/faroub)
+ * @date March 2020
+*/
+
+int main(void) {
+
+  // Init
+
+  // Mainloop
+  while (1) {
+
+
+  }
+  return 0;
+}
+
+
+
+
