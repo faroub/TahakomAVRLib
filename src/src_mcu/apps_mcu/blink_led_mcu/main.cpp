7c127bfd14be96aaf717b5ec3467a65e7f5059c1
Farid Oubbati <f.oubbati@yahoo.f>
2020-06-22 16:02:12 +0200

    complete main function of the blinkled application

--- src/src_mcu/apps_mcu/blink_led_mcu/main.cpp
+++ src/src_mcu/apps_mcu/blink_led_mcu/main.cpp
@@ -4,19 +4,33 @@
  * @author Farid Oubbati (https://github.com/faroub)
  * @date March 2020
 */
-
 #include "Led.h"
+#include <util/delay.h>
+
+#define PIN_NUMBER 1  /**< pin number to which a Led is connected */
+#define DELAYTIME 1000 /**< time delay */
+
 
 
 int main(void) {
 
   // Init
 
-  component::Led testLed(io::Pin(2,io::PortB));
+
+  // instantiate a Led object
+  component::Led Led(io::Pin(PIN_NUMBER,io::PortB));
+
 
 
   // Mainloop
   while (1) {
+
+
+      Led.on();
+      _delay_ms(DELAYTIME);
+
+      Led.off();
+      _delay_ms(DELAYTIME);
 
 
   }
