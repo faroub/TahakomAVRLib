d59c033682564ad143277e19eb68fc83e5b30f35
Farid Oubbati <f.oubbati@yahoo.f>
2020-06-23 21:35:29 +0200

    complete header ha_m328p.h header file

--- src/src_mcu/lib_mcu/utils/ha_m328p.h
+++ src/src_mcu/lib_mcu/utils/ha_m328p.h
@@ -35,32 +35,79 @@
     #error "too high systematic baud rate error, greater than 1%!"
 #endif
 
-# define   USART0_SET_BAUDRATE_HIGH_REGISTER  UBRR0H = UBRR_VAL >> 8;
-# define   USART0_SET_BAUDRATE_LOW_REGISTER   UBRR0L = UBRR_VAL & 0xFF;
-# define   USART0_DATA_REGISTER           UDR0
+#define   USART0_SET_BAUDRATE_HIGH_REGISTER     UBRR0H = UBRR_VAL >> 8;
+#define   USART0_SET_BAUDRATE_LOW_REGISTER      UBRR0L = UBRR_VAL & 0xFF;
 
-# define   USART0_SET_8B1SNP              UCSRC |= 1 << URSEL | 1 << UCSZ1 | 1 << UCSZ0
+#define   USART0_DATA_REGISTER UDR0
 
-# define   USART0_ENABLE_SENDER           UCSR0B |=   1 << TXEN0
-# define   USART0_DISABLE_SENDER          UCSR0B &= ~(1 << TXEN0)
-# define   USART0_ENABLE_SENDER_INTERRUPT   UCSR0B |=   1 << UDRIE0
-# define   USART0_DISABLE_SENDER_INTERRUPT  UCSR0B &= ~(1 << UDRIE0)
+#define   USART0_ENABLE_ASYNC_TRANSMISSION_MODE     UCSR0C &= ~((1 << UMSEL01) | (1 << UMSEL00))
+#define   USART0_ENABLE_SYNC_TRANSMISSION_MODE      UCSR0C = (UCSR0C & ~(1 << UMSEL01)) | (1 << UMSEL00)
+#define   USART0_ENABLE_MASTER_SPI_MODE             UCSR0C |= (1 << UMSEL01) | (1 << UMSEL00)
 
-# define   USART0_ENABLE_RECEIVER         UCSR0B |=   1 << RXEN0
-# define   USART0_DISABLE_RECEIVER        UCSR0B &= ~(1 << RXEN0)
 
-# define   USART0_ENABLE_RECEIVER_INTERRUPT   UCSR0B |=   1 << RXCIE0
-# define   USART0_DISABLE_RECEIVER_INTERRUPT  UCSR0B &= ~(1 << RXCIE0)
+#define   USART0_ENABLE_DOUBLE_SPEED_MODE          UCSR0A |= (1 << U2X0)
+#define   USART0_DISABLE_DOUBLE_SPEED_MODE         UCSR0A &= ~(1 << U2X0);
 
-# define   USART0_FRAME_ERROR             FE
 
-# define   USART0_DATA_OVERRUN            DOR
+#define   USART0_ENABLE_EVEN_PARITY_MODE    UCSR0C = (1 << UPM01) | (UCSR0C & ~(1 << UPM00))
+#define   USART0_ENABLE_ODD_PARITY_MODE     UCSR0C |=   ((1 << UPM01) | (1 << UPM00))
+#define   USART0_DISABLE_PARITY_MODE        UCSR0C &= ~((1 << UPM01) | (1 << UPM00))
 
-# define   USART0_PARITY_ERROR            PE
 
-# define   USART0_RECEIVE_INTERRUPT       USART_RXC_vect
+#define   USART0_SET_ONE_STOP_BIT   UCSR0C &= ~(1 << USBS0)
+#define   USART0_SET_TWO_STOP_BITS   UCSR0C |= (1 << USBS0)
 
-# define   USART0_SENDER_INTERRUPT        USART_UDRE_vect
+
+#define   USART0_SET_9BIT_FRAME_SIZE   \
+    do { \
+        UCSR0C |= ((1 << UCSZ01) | (1 << UCSZ00)); \
+        UCSR0B |= (1 << UCSZ02) \
+    } while (0)
+
+#define   USART0_SET_8BIT_FRAME_SIZE   UCSR0C  \
+    do { \
+        UCSR0C |= ((1 << UCSZ01) | (1 << UCSZ00)); \
+        UCSR0B &= ~(1 << UCSZ02) \
+    } while (0)
+
+#define   USART0_SET_7BIT_FRAME_SIZE  \
+    do { \
+        UCSR0C = (1 << UCSZ01) | (UCSR0C & ~(1 << UCSZ00)); \
+        UCSR0B &= ~(1 << UCSZ02) \
+    } while (0)
+#define   USART0_SET_6BIT_FRAME_SIZE \
+    do { \
+        UCSR0C = (UCSR0C & ~(1 << UCSZ01)) | (1 << UCSZ00); \
+        UCSR0B &= ~(1 << UCSZ02) \
+    } while (0)
+#define   USART0_SET_5BIT_FRAME_SIZE \
+    do { \
+        UCSR0C &= ~((1 << UCSZ01) | (1 << UCSZ00)); \
+        UCSR0B &= ~(1 << UCSZ02) \
+    } while (0)
+
+
+#define   USART0_ENABLE_SENDER           UCSR0B |=   1 << TXEN0
+#define   USART0_DISABLE_SENDER          UCSR0B &= ~(1 << TXEN0)
+
+#define   USART0_ENABLE_SENDER_INTERRUPT   UCSR0B |=   1 << UDRIE0
+#define   USART0_DISABLE_SENDER_INTERRUPT  UCSR0B &= ~(1 << UDRIE0)
+
+#define   USART0_ENABLE_RECEIVER         UCSR0B |=   1 << RXEN0
+#define   USART0_DISABLE_RECEIVER        UCSR0B &= ~(1 << RXEN0)
+
+#define   USART0_ENABLE_RECEIVER_INTERRUPT   UCSR0B |=   1 << RXCIE0
+#define   USART0_DISABLE_RECEIVER_INTERRUPT  UCSR0B &= ~(1 << RXCIE0)
+
+#define   USART0_FRAME_ERROR             FE0
+
+#define   USART0_DATA_OVERRUN            DOR0
+
+#define   USART0_PARITY_ERROR            UPE0
+
+#define   USART0_RECEIVE_INTERRUPT       USART_RXC_vect
+
+#define   USART0_SENDER_INTERRUPT        USART_UDRE_vect
 
 #endif
 #endif
