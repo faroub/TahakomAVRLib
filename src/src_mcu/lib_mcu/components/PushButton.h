674fc80f015bf91f4ad5aeb2215e32536181c518
Farid Oubbati <f.oubbati@yahoo.f>
2020-07-01 14:17:37 +0200

    add PushButton class files

---
+++ src/src_mcu/lib_mcu/components/PushButton.h
@@ -0,0 +1,28 @@
+/**
+ * @file PushButton.h
+ * @brief Header file of the Push Button class
+ *
+ * class to monitor a Push Button
+ * @author Farid Oubbati (https://github.com/faroub)
+ * @date March 2020
+*/
+#ifndef PUSHBUTTON_H
+#define PUSHBUTTON_H
+
+namespace component
+{
+class PushButton
+{
+public:
+
+protected:
+
+private:
+
+};
+}
+
+
+
+
+#endif
