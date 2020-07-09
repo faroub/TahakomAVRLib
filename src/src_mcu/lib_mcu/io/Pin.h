af5714a44883f4afc02bd6139d333213f2096fe7
Farid Oubbati <f.oubbati@yahoo.f>
2020-07-09 19:21:13 +0200

    modify header comments

--- src/src_mcu/lib_mcu/io/Pin.h
+++ src/src_mcu/lib_mcu/io/Pin.h
@@ -2,7 +2,7 @@
  * @file Pin.h
  * @brief Header file of the Pin class
  *
- * Basic class for IO abstraction of the Atmega328p Pin and Port
+ * Basic class for IO abstraction of Pin and Port
  * @author Farid Oubbati (https://github.com/faroub)
  * @date March 2020
 */
