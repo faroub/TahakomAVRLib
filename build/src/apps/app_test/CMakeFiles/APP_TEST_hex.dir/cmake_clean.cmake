d49ed0990cd4115297410dc558cddbece889aa06
Farid Oubbati <f.oubbati@yahoo.f>
2020-08-21 16:38:06 +0200

    remove build directory

--- build/src/apps/app_test/CMakeFiles/APP_TEST_hex.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/APP_TEST_hex"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/APP_TEST_hex.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/src/apps/app_test/CMakeFiles/APP_TEST_strip.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/APP_TEST_strip"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/APP_TEST_strip.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
