cd605f953e7abbe6e92b8f1fc6b544e2fd0d5f8d
Farid Oubbati <f.oubbati@yahoo.f>
2020-10-02 13:11:54 +0200

    test Sleeping Modes

--- src/apps/app_test/main.cpp
+++ src/apps/app_test/main.cpp
@@ -5,121 +5,60 @@
  * @date March 2020
 */
 
-#include "WatchdogTimer.h"
+#include "PushButton.h"
 #include "Led.h"
-// instantiate a Led object
-extern component::Led Led;
-component::Led Led(io::Pin(1,io::PortB));
-extern component::Led LedStart;
-component::Led LedStart(io::Pin(2,io::PortB));
+#include "ExternInterrupt.h"
+#include "MCU.h"
 
+#define PUSHBUTTON_NUMBER 2
+#define LED_NUMBER 0
 
 int main(void) {
 
-   // Init
-   // instantiate a Watchdog object
-   core::WatchdogTimer &myWatchdog = core::WatchdogTimer::getInstance();
-   myWatchdog.selectTimeOut(core::timeOut::to_8s);
-   LedStart.on();
-   _delay_ms(5000);
-   LedStart.off();
-   myWatchdog.start(core::operationMode::reset);
-   // ------ Event loop ------ //
-   while (1) {
+    // Init
+    // initialize MCU
+    core::MCU::init();
 
-       Led.on();
-       _delay_ms(1000);
-       Led.off();
-       _delay_ms(1000);
+    // instantiate a Led object
+    component::Led Led(io::Pin(LED_NUMBER,io::PortB));
 
-   }
-   return 0;
-}
+    // instantiate a Led object
+    component::PushButton PushButton(io::Pin(PUSHBUTTON_NUMBER,io::PortD));
 
-void core::WatchdogTimer::timeOutServiceRoutine()
-{
-    for (uint8_t i=0;i<10;i++)
-    {
+    // instantiate the external interrupt manager
+    core::ExternInterrupt &myExternInterrupt = core::ExternInterrupt::getInstance();
+    myExternInterrupt.enableInt0(1);
+    myExternInterrupt.setInt0SenseControl(core::senseControl::logicalChange);
+
+    // set sleep mode
+    core::MCU::selectSleepMode(core::sleepMode::powerDown);
+
+
+    // Mainloop
+    while (1) {
+
+        //flash the LED
+        for (uint8_t i=0;i<10;i++)
+        {
+            Led.on();
+            _delay_ms(100);
+            Led.off();
+            _delay_ms(100);
+        }
+        _delay_ms(5000);
         Led.on();
         _delay_ms(100);
         Led.off();
-        _delay_ms(100);
+        core::MCU::goToSleep(core::BODMode::enabled);
 
     }
+    return 0;
+}
+
+void core::ExternInterrupt::Int0ServiceRoutine()
+{
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-//#include "ADC.h"
-//#include "USART0.h"
-
-
-
-
-//#define TRANSMIT_BUFFER_SIZE 7
-
-
-
-//int main(void) {
-
-//   // Init
-
-
-//   // instantiate the USART0 object
-//   io::USART0 &myUSART0 = io::USART0::getInstance();
-//   // transmit data buffer
-//   char l_data2Send[TRANSMIT_BUFFER_SIZE];
-
-//   // instantiate the ADC object
-//   core::ADConverter &myADC = core::ADConverter::getInstance();
-
-
-//   // select analog input
-//   myADC.selectAnalogInput(io::Pin(0,io::PortC));
-
-//   // variable to hold conversion result
-//   uint16_t l_conversionResult = 0;
-
-//   // enable and start conversion
-//   myADC.start();
-
-//   // ------ Event loop ------ //
-//   while (1) {
-
-
-
-//       myADC.getConversionResult(&l_conversionResult, core::resolution::RES_16bit);
-
-//       if (myADC.conversionComplete())
-//       {
-//           l_data2Send[0] = '0' + (l_conversionResult / 10000);
-//           l_data2Send[1] = '0' + ((l_conversionResult / 1000) % 10);
-//           l_data2Send[2] = '0' + ((l_conversionResult / 100) % 10);
-//           l_data2Send[3] = '0' + ((l_conversionResult / 10) % 10);
-//           l_data2Send[4] = '0' + (l_conversionResult % 10);
-//           l_data2Send[5] = '\n';
-//           l_data2Send[6] = '\r';
-
-//           if (myUSART0.ready2Send())
-//           {
-//               myUSART0.sendFrame(reinterpret_cast<uint8_t*>(l_data2Send),TRANSMIT_BUFFER_SIZE);
-//           }
-
-//       }
-//   }
-//   return 0;
-//}
-
-
 
 
