2f498b47841d320adef8d2852dbb840e62f75220
Farid Oubbati <f.oubbati@yahoo.f>
2020-10-19 21:16:54 +0200

    add a seperate selectClockSource member function

--- src/lib/core/TimerCounter0.cpp
+++ src/lib/core/TimerCounter0.cpp
@@ -23,8 +23,9 @@ core::TimerCounter0::TimerCounter0(const channel &ar_channel,
                                    const compareOutputMode& ar_compareOutputMode)
 {
     core::MCU::enableTimerCounter0(1);
+    stop();
     selectOperationMode(ar_operationMode);
-    start(ar_clockSource);
+    selectClockSource(ar_clockSource);
     selectCompareOutputMode(ar_channel,ar_compareOutputMode);
 
 
@@ -35,6 +36,62 @@ core::TimerCounter0::~TimerCounter0()
 
 }
 
+void core::TimerCounter0::selectClockSource(const clockSource &ar_clockSource)
+{
+    switch (ar_clockSource)
+    {
+        case core::clockSource::noClock:
+        {
+            m_clockPrescaler=0;
+            m_clockSource=0;
+            break;
+        }
+        case core::clockSource::PS_1:
+        {
+            m_clockPrescaler=1;
+            m_clockSource=1;
+            break;
+        }
+        case core::clockSource::PS_8:
+        {
+            m_clockPrescaler=8;
+            m_clockSource=2;
+            break;
+        }
+        case core::clockSource::PS_64:
+        {
+            m_clockPrescaler=64;
+            m_clockSource=3;
+            break;
+        }
+        case core::clockSource::PS_256:
+        {
+            m_clockPrescaler=256;
+            m_clockSource=4;
+            break;
+        }
+        case core::clockSource::PS_1024:
+        {
+            m_clockPrescaler=1024;
+            m_clockSource=5;
+            break;
+        }
+        case core::clockSource::extern_Clock_T0_Falling_Edge:
+        {
+            m_clockPrescaler=0;
+            m_clockSource=6;
+            break;
+        }
+        case core::clockSource::extern_Clock_T0_Rising_Edge:
+        {
+            m_clockPrescaler=0;
+            m_clockSource=7;
+            break;
+        }
+    }
+
+
+}
 
 void core::TimerCounter0::selectOperationMode(const operationMode &ar_operationMode)
 {
@@ -76,52 +133,14 @@ void core::TimerCounter0::selectOperationMode(const operationMode &ar_operationM
 }
 
 
-void core::TimerCounter0::start(const clockSource &ar_clockSource)
+void core::TimerCounter0::start()
 {
-    switch (ar_clockSource)
-    {
-        case core::clockSource::noClock:
-        {
-            TIMER0_SELECT_CLOCK_SOURCE(0);
-            break;
-        }
-        case core::clockSource::PS_1:
-        {
-            TIMER0_SELECT_CLOCK_SOURCE(1);
-            break;
-        }
-        case core::clockSource::PS_8:
-        {
-            TIMER0_SELECT_CLOCK_SOURCE(2);
-            break;
-        }
-        case core::clockSource::PS_64:
-        {
-            TIMER0_SELECT_CLOCK_SOURCE(3);
-            break;
-        }
-        case core::clockSource::PS_256:
-        {
-            TIMER0_SELECT_CLOCK_SOURCE(4);
-            break;
-        }
-        case core::clockSource::PS_1024:
-        {
-            TIMER0_SELECT_CLOCK_SOURCE(5);
-            break;
-        }
-        case core::clockSource::extern_Clock_T0_Falling_Edge:
-        {
-            TIMER0_SELECT_CLOCK_SOURCE(6);
-            break;
-        }
-        case core::clockSource::extern_Clock_T0_Rising_Edge:
-        {
-            TIMER0_SELECT_CLOCK_SOURCE(7);
-            break;
-        }
-    }
+    TIMER0_SELECT_CLOCK_SOURCE(m_clockSource);
+}
 
+uint16_t core::TimerCounter0::getClockPrescaler()
+{
+    return m_clockPrescaler;
 }
 
 void core::TimerCounter0::stop()
