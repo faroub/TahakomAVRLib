6b8e49a3de511c49f6c2436a99a7d9eac3bb4578
Farid Oubbati <f.oubbati@yahoo.f>
2021-03-09 14:14:32 +0100

    add stepper motor test application

--- src/apps/app_test/main.cpp
+++ src/apps/app_test/main.cpp
@@ -6,108 +6,77 @@
 */
 
 
+#include "StepperMotor.h"
+#include "USART0.h"
 #include "MCU.h"
-#include "TimerCounter0.h"
-#include "DCMotor.h"
 
-#define DCMOTOR_NUMBER 6
-#define DCMOTOR_BACKWARD 0
-#define DCMOTOR_FORWARD 1
+#define StepperMotor_Pin_1 0
+#define StepperMotor_Pin_2 1
+#define StepperMotor_Pin_3 2
+#define StepperMotor_Pin_4 3
+
+#define STEP_ANGLE_FULL_STEP 0.176
+#define STEP_ANGLE_HALF_STEP 0.0879
+
+
 
 int main(void) {
 
-   // Init
-   // initialize MCU
-   core::MCU::init();
+    // initialize MCU
+    core::MCU::init();
 
-   // instantiate the TimerCounter0 object
-   core::TimerCounter0 &myTimerCounter0 = core::TimerCounter0::getInstance();
-   myTimerCounter0.selectClockSource(core::clockSource::PS_256);
+    // instantiate the USART0 object
+    io::USART0 &myUSART0 = io::USART0::getInstance();
 
-   // instantiate a DCMotor object
-   component::DCMotor myDCMotor(io::Pin(DCMOTOR_NUMBER,io::PortD));
-
-   // instantiate direction pins objects
-
-   io::Pin myPinForward = io::Pin(DCMOTOR_FORWARD,io::PortB);
-   myPinForward.toOutput();
-
-   io::Pin myPinBackward = io::Pin(DCMOTOR_BACKWARD,io::PortB);
-   myPinBackward.toOutput();
+    myUSART0.sendString("Stepper Motor Speed!\r\n");
 
 
-   myDCMotor.connect(myTimerCounter0);
 
-   myPinForward.setHigh();
-   myPinBackward.setLow();
-   myDCMotor.spin(myTimerCounter0,30);
-   _delay_ms(5000);
+    // instantiate the StepperMotor object
+    component::StepperMotor myStepperMotor(component::mode::halfStep,
+                                           io::Pin(StepperMotor_Pin_1,io::PortB),
+                                           io::Pin(StepperMotor_Pin_2,io::PortB),
+                                           io::Pin(StepperMotor_Pin_3,io::PortB),
+                                           io::Pin(StepperMotor_Pin_4,io::PortB));
 
-   myPinForward.setLow();
-   myPinBackward.setHigh();
-   myDCMotor.spin(myTimerCounter0,30);
-   _delay_ms(5000);
+    int16_t l_angle = 180;
+    int16_t l_step = static_cast<int16_t>(l_angle / STEP_ANGLE_HALF_STEP);
 
-   myDCMotor.disconnect(myTimerCounter0);
+    // set speed [steps/s]
+    // full step: max speed: 500p/s
+    // half step: max speed: 1000p/s
+   
+
+    // set speed [°/s]
+    // full step: max speed: 40°/s
+    // half step: max speed: 80°/s
+
+//    myStepperMotor.computeStepDelay(3095, 1000, 1000, 1000);
+//    myUSART0.sendWord(myStepperMotor.m_accelTime);
+//    myUSART0.sendString("\r\n");
+//    myUSART0.sendWord(myStepperMotor.m_constSpeedTime);
+//    myUSART0.sendString("\r\n");
+//    myUSART0.sendWord(myStepperMotor.m_decelTime);
+//    myUSART0.sendString("\r\n");
 
 
    // Mainloop
    while (1) {
 
+        while (!myStepperMotor.goalReached())
+        {
+            // set motor steps
+            //myStepperMotor.step(l_step,1000);
+            //myStepperMotor.step(l_step,80,STEP_ANGLE_HALF_STEP);
+            myStepperMotor.step(3095, 1000, 1000, 1000);
+//            myUSART0.sendLong(myStepperMotor.m_temp1);
+//            myUSART0.sendString("\t\t");
+//            myUSART0.sendLong(myStepperMotor.m_temp2);
+//            myUSART0.sendString("\r\n");
 
-
-
-
+        }
    }
    return 0;
 }
 
 
-
-//#include "MCU.h"
-//#include "TimerCounter0.h"
-//#include "DCMotor.h"
-
-//#define DCMOTOR_NUMBER 6
-
-//int main(void) {
-
-//   // Init
-//   // initialize MCU
-//   core::MCU::init();
-
-//   // instantiate the TimerCounter0 object
-//   core::TimerCounter0 &myTimerCounter0 = core::TimerCounter0::getInstance();
-//   myTimerCounter0.selectClockSource(core::clockSource::PS_1024);
-
-//   // instantiate a DCMotor object
-//   component::DCMotor myDCMotor(io::Pin(DCMOTOR_NUMBER,io::PortD));
-
-
-//   myDCMotor.connect(myTimerCounter0);
-
-//   myDCMotor.spin(myTimerCounter0,0);
-//   _delay_ms(5000);
-
-//   myDCMotor.spin(myTimerCounter0,75);
-//   _delay_ms(5000);
-
-//   myDCMotor.spin(myTimerCounter0,190);
-//   _delay_ms(5000);
-
-//   myDCMotor.spin(myTimerCounter0,200);
-//   _delay_ms(5000);
-
-//   myDCMotor.disconnect(myTimerCounter0);
-
-
-//   // Mainloop
-//   while (1) {
-
-
-
-
-
-//   }
-//   return 0;
-//}
