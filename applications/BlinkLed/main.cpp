83f80257e6611988e9ca5d9b949f53f3e7a09b2a
Farid Oubbati <f.oubbati@yahoo.f>
2021-03-11 15:19:29 +0100

    clean main.cpp

--- applications/BlinkLed/main.cpp
+++ applications/BlinkLed/main.cpp
@@ -5,43 +5,35 @@
  * This example demonstrates the use of the Led and Pin abstraction objects.
  *
  * - File:               main.cpp
- * - Compiler:           avr
- * - Supported devices:  The example is written for the ATmega382p
+ * - Compiler:           gcc-avr
+ * - Supported devices:  The example compiles on the ATmega48P/88P/168P/328P MCU family
  *
  *
  * @author Farid Oubbati (https://github.com/faroub)
- * @date March 2020
+ * @date March 2018
 */
 
 #include "Led.h"
 #include <util/delay.h>
 
-#define PIN_NUMBER 1  /**< Led pin number */
-#define TIMEDELAY 1000 /**< Time delay */
-
+#define PIN_NUMBER 0  /**< Led pin number */
+#define TIMEDELAY 500 /**< Time delay */
 
 int main(void) {
 
   // Init
 
-
   // Instantiate a Led object
   component::Led Led(io::Pin(PIN_NUMBER,io::PortB));
 
-
-
   // Mainloop
   while (1) {
-
 
       Led.on();
       _delay_ms(TIMEDELAY);
 
       Led.off();
       _delay_ms(TIMEDELAY);
-
-
   }
   return 0;
 }
-
